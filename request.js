c8a00542706d30c66eb8804a81e012cc09ef9b44
KiHyeon-Hong <ghdrlgus96@gmail.com>
2022-02-09 11:41:08 +0900

    Server notification update

---
+++ request.js
@@ -0,0 +1,13 @@
+const request = require('request');
+const ip = require('ip');
+const fs = require('fs');
+
+const addr = fs.readFileSync(__dirname + '/files/serverAddress.txt', 'utf8');
+
+request.get({ url: 'https://api.ipify.org' }, function (_1, _2, body) {
+  console.log('Public IP address > ' + body);
+  console.log('Virtual IP address > ' + ip.address());
+
+  request.get({ url: `${addr}?pri=${ip.address()}&pub=${body}` }, function (_1, _2, body) {});
+
+});
